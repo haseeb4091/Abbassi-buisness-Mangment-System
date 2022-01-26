1fcf5e8a617a28c6238003bc9d429a59bf8ca6f9
haseeb4091 <jasialnew1@gmail.com>
2022-01-26 15:11:28 +0500

    Fourth

--- Abbassi buisness Mangment System/RecievePayments.cs
+++ Abbassi buisness Mangment System/RecievePayments.cs
@@ -12,8 +12,21 @@ namespace Abbassi_buisness_Mangment_System
 {
     public partial class RecievePayments : UserControl
     {
+        private static RecievePayments _instance;
+        public static RecievePayments Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new RecievePayments();
+                }
+                return _instance;
+            }
+        }
         public RecievePayments()
         {
+            Home.con.Open();
             InitializeComponent();
         }
     }
